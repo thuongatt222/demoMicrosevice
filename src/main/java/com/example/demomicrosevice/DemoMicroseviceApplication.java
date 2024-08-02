36509bd97dadd39a671e458d7cbf5e96bc5f6ff1
thuongatt222 <thuongatt2k3@gmail.com>
2024-08-02 14:33:49 +0700

    commit

---
+++ src/main/java/com/example/demomicrosevice/DemoMicroseviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomicrosevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroseviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMicroseviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demomicrosevice/DemoMicroseviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomicrosevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroseviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
